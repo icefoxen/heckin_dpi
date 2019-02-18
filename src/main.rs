ec7591e6fc4e5428936785d90d762706f5143811
Simon Heath <icefoxen@gmail.com>
2019-02-18 14:43:04 -0500

    Draw more lines that are always on pixel boundaries.
    Not always an improvement, but it makes things that are >= 1 pixel
    display nicely.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ struct MainState {
     offset: f32,
 }
 
+const WINDOW_WIDTH: f32 = 1024.0;
+
 impl MainState {
     fn new(ctx: &mut ggez::Context) -> ggez::GameResult<Self> {
         let mesh1 = graphics::MeshBuilder::new()
@@ -49,6 +51,10 @@ impl ggez::event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult<()> {
         graphics::clear(ctx);
         graphics::draw(ctx, &self.mesh1, na::Point2::new(self.offset, 0.0), 0.0)?;
+        // graphics::draw(ctx, &self.mesh1, na::Point2::new(0.0, 0.0), self.offset / 100.0)?;
+
+        let pixel_perfect_offset = f32::floor(self.offset) + 25.0;
+        graphics::draw(ctx, &self.mesh1, na::Point2::new(pixel_perfect_offset, 300.0), 0.0)?;
         graphics::present(ctx);
         Ok(())
     }
@@ -56,6 +62,9 @@ impl ggez::event::EventHandler for MainState {
 
 fn main() {
     let cb = ggez::ContextBuilder::new("heckin_dpi", "me")
+        .window_setup(ggez::conf::WindowSetup::default()
+            // .samples(8).unwrap()
+            .allow_highdpi(true))
         .window_mode(ggez::conf::WindowMode::default().dimensions(1024, 768));
     let ctx = &mut cb.build().unwrap();
     let main = &mut MainState::new(ctx).unwrap();
